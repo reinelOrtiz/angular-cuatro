e0f9e6900df018fcf1441d3750cf649d2c6bcc8a
rort <rort@rort.com>
2019-07-03 16:19:59 -0500

    -Mejoramiento de estructura, carpetas comunes, públicas y privadas\-Creación componente de loader (spinkit)\n-Creación componente header\n-Creación componente de lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './private/project-list/project-list.component';
+import { HeaderComponent } from './commons/header/header.component';
+import { LoaderComponent } from './commons/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    HeaderComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule
